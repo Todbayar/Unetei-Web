494f82fd515d47e20ae6be3c3edda7e09936f25f
Todbayar <atodko0513@gmail.com>
2023-03-15 16:36:03 +0800

    signup with phone number

--- index.php
+++ index.php
@@ -4,13 +4,18 @@
 		<meta charset="utf-8">
 		<title>Unetei</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		
+		<script src="https://www.gstatic.com/firebasejs/9.12.1/firebase-app-compat.js"></script>
+		<script src="https://www.gstatic.com/firebasejs/9.12.1/firebase-auth-compat.js"></script>
 		<script src="jquery-3.6.4.min.js"></script>
+		<script src="https://kit.fontawesome.com/64e3bec699.js" crossorigin="anonymous"></script>
 		<script src="misc.js"></script>
+		
 		<link rel="stylesheet" href="main.css">
 		<link rel="stylesheet" href="topbar.css">
 		<link rel="stylesheet" href="buttons.css">
 		<link rel="stylesheet" href="dropdowns.css">
-		<script src="https://kit.fontawesome.com/64e3bec699.js" crossorigin="anonymous"></script>
+
 		<style>
 			body {
 				border: 0;
@@ -20,7 +25,33 @@
 			}
 		</style>
 		<script>
+			const firebaseConfig = {
+				apiKey: "AIzaSyAJxCfAZKgG4vy_nd6UVX3UKhZAF0iyKl4",
+				authDomain: "unetei-bc717.firebaseapp.com",
+				projectId: "unetei-bc717",
+				storageBucket: "unetei-bc717.appspot.com",
+				messagingSenderId: "834168591977",
+				appId: "1:834168591977:web:ee2f1d66da1dcd0e33b4f8",
+				measurementId: "G-Q1YLJDB3KC"
+			};
+			firebase.initializeApp(firebaseConfig);
+			
 			$(document).ready(function(){
+				firebase.auth().onAuthStateChanged(function(user) {
+					if (user) {
+						// User is signed in.
+						var uid = user.uid;
+						var email = user.email;
+						var photoURL = user.photoURL;
+						var phoneNumber = user.phoneNumber;
+						var isAnonymous = user.isAnonymous;
+						var displayName = user.displayName;
+						var providerData = user.providerData;
+						var emailVerified = user.emailVerified;
+						alert(uid + ", " + email + ", " + phoneNumber);
+					}
+				});
+												   
 				$(".myzar").hover(function(){
 					$(".dropdown").show();
 				}, function(){
--- login.php
+++ login.php
@@ -1,24 +1,80 @@
 <div style="padding-left: 20px; padding-right: 20px; padding-top: 0px; width: 100%">
 	<p style="font: bold 24px Arial">Нэвтрэх</p>
 	<hr/>
-	<p style="font: bold 18px Arial">Таны утасны дугаар</p>
-	<div style="display: flex; align-items: center">
-		<div style="font: bold 18px Arial">+976</div>
-		<input 
-			   type="tel" 
-			   id="phone" 
-			   name="phone" 
-			   placeholder="_ _ _ _ _ _ _ _" 
-			   pattern="[0-9]{8}"
-			   maxlength="8"
-			   style="margin-left: 10px; height: 30px; padding: 5px; font: bold 18px Arial">
+	<div id="phoneentry_container">
+		<p style="font: bold 18px Arial">Таны утасны дугаар</p>
+		<div style="display: flex; align-items: center">
+			<div style="font: bold 18px Arial">+976</div>
+			<input 
+				   type="tel" 
+				   id="loginPhone" 
+				   placeholder="_ _ _ _ _ _ _ _" 
+				   pattern="[0-9]{8}"
+				   maxlength="8"
+				   style="margin-left: 10px; height: 30px; padding: 5px; font: bold 18px Arial">
+		</div>
+		<div style="margin-top: 10px; display: flex; align-items: center">
+			<input id="loginAgree" type="checkbox">
+			<p style="margin-left: 10px">Би Unetei.mn сайтын үйлчилгээний нөхцөл, зар нийтлэх журмыг хүлээн зөвшөөрч, мөн өөрийгөө 18 нас хүрсэн болохыг баталж байна.</p>
+		</div>
+		<div id="recaptcha-container"></div>
+		<div>
+			<p id="loginError" style="color: #FF0004">Та үйлчилгээний нөхцөлийг зөвшөөрснөө баталж чагтална уу!</p>
+			<button id="loginButton" class="button_yellow" type="button" style="font: normal 18px Arial" onClick="phoneAuth()">Мессежээр нэвтрэх</button>
+		</div>
 	</div>
-	<div style="margin-top: 10px; display: flex; align-items: center">
-		<input type="checkbox" id="agree" name="agree" value="agree">
-		<p style="margin-left: 10px">Би Unetei.mn сайтын үйлчилгээний нөхцөл, зар нийтлэх журмыг хүлээн зөвшөөрч, мөн өөрийгөө 18 нас хүрсэн болохыг баталж байна.</p>
-	</div>
-	<div>
-		<p>Эхлээд та үйлчилгээний нөхцөлийг зөвшөөрснөө чагтална уу</p>
-		<button class="button_yellow" type="button" enabled style="font: normal 18px Arial">Мессежээр нэвтрэх</button>
+	<div id="phoneverifier_container" style="display: none">
+		<div>Таны утасруу баталгаажуулах код бүхий мессеж илгээгдлээ.<p id="phoneverifier_number"></p></div>
+		<p>Доорх талбарт код оруулна уу.</p>
+		<input type="text" id="phoneverificationcode" maxlength="6" pattern="[0-9]{6}" placeholder=" _ _ _ _ _ _" style="height: 30px; padding: 5px; font: bold 18px Arial">
+		<br/>
+		<p id="loginVerificationError" style="color: #FF0004"></p>
+		<input type="button" id="verify" value="Үргэлжлүүлэх"  class="button_yellow" style="font: normal 18px Arial" onClick="phoneCodeVerify()">
 	</div>
 </div>
+
+<script>
+	render();
+	function render() {
+		window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier('recaptcha-container');
+		recaptchaVerifier.render();
+	}
+	
+	function phoneAuth() {
+		var vPhoneRegex = new RegExp("^[0-9]{8}$");
+		const vPhone = $("#loginPhone").val();
+		const vAgree = $("#loginAgree").prop('checked');
+		const vError = $("#loginError");
+
+		if(vPhoneRegex.test(vPhone) && vAgree){
+			const vValidPhone = "+976" + vPhone;
+			firebase.auth().signInWithPhoneNumber(vValidPhone, window.recaptchaVerifier).then(function (confirmationResult) {
+				window.confirmationResult = confirmationResult;
+				coderesult = confirmationResult;
+				$("#phoneentry_container").hide();
+				$("#phoneverifier_container").show();
+				$("#phoneverifier_number").text(vValidPhone);
+			}).catch(function (error) {
+				vError.text(error.message);
+			});
+		}
+		else if(!vAgree && !vPhoneRegex.test(vPhone)) {
+			vError.text("Та утасны дугаараа оруулаад үйлчилгээний нөхцөлийг чагталж зөвшөөрнө үү!");
+		}
+		else if(!vPhoneRegex.test(vPhone)) {
+			vError.text("Та утасны дугаараа зөв оруулна уу!");
+		}
+		else {
+			vError.text("Та үйлчилгээний нөхцөлийг зөвшөөрснөө баталж чагтална уу!");
+		}
+	}
+	
+	function phoneCodeVerify(){
+		var code = document.getElementById('phoneverificationcode').value;
+		coderesult.confirm(code).then(function(){
+			alert("Boljee");
+		}).catch(function(){
+			$("#loginVerificationError").text("Оруулсан баталгаажуулах код буруу байна, та дахин оролдоно уу!");			
+		});
+	}
+</script>
