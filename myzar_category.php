bbefdb103629ab846bed37ccc3774efa59ec8597
Todbayar <atodko0513@gmail.com>
2023-03-16 18:09:33 +0800

    myzar category -update category add popup

--- myzar_category.php
+++ myzar_category.php
@@ -1,3 +1,24 @@
+<style>
+	.popup {
+		background:#E8E8E8;
+		position:relative;
+		border-radius: 10px;
+		padding-bottom: 10px;
+		top: 50px;
+		box-shadow: 5px 5px 5px lightblue;
+	}
+	
+	.popup .header {
+		border-top-left-radius: 10px;
+		border-top-right-radius: 10px;
+		background: #42c200;
+		text-align: center;
+		height: 30px;
+		position:relative;
+		
+	}
+</style>
+
 <script>
 	function myzar_category_add_show(){
 		
@@ -14,4 +35,13 @@
 		<div style="margin-left: 5px">Нэмэх</div>
 		<i class="fa-solid fa-circle-minus" style="color:#FF4649; margin-left: 10px; font-size: 20px"></i>
 	</div>
+</div>
+
+<div class="popup" style="margin: auto; width: 240px">
+	<div class="header">Ангилал</div>
+	<div style="display: flex; align-items: center; margin: 10px; height: 50px">
+		<img src="image-solid.svg" width="24" height="24" />
+		<input type="text" maxlength="20" placeholder="Бичих..." style="margin-left: 5px" />
+	</div>
+	<button class="button_yellow" style="margin-top: 10px; margin-left: auto; margin-right: auto">Илгээх</button>
 </div>
