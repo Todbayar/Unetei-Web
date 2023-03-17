7923fed99aaad301bd3084f01bdda19177399f59
Todbayar <atodko0513@gmail.com>
2023-03-17 21:09:06 +0800

    myzar category add xmlhttprequest with firebase recaptchaVerifier

--- myzar_category.php
+++ myzar_category.php
@@ -1,20 +1,86 @@
 <script>
+	var isMyZarCategoryAddIconValid = false;
+	var isMyZarCategoryAddTitleValid = false;
+	
 	$(document).ready(function(){
 		$("#myzar_category_add_icon").change(function(){
-			const vIconType = $("#myzar_category_add_icon")[0].files[0].type;
-			const vIconName = $("#myzar_category_add_icon")[0].files[0].name;
-			console.log($("#myzar_category_add_icon")[0].files[0].name);
+			vIconType = $("#myzar_category_add_icon")[0].files[0].type;
+			vIconName = $("#myzar_category_add_icon")[0].files[0].name;
 			if(vIconType == "image/svg+xml" || vIconType == "image/png"){
-				$("#myzar_category_add_icon_file").text("Файл:" + vIconName);
+				$("#myzar_category_add_msg").text("Файл:" + vIconName);
 				document.getElementById("myzar_category_add_icon_image").src = URL.createObjectURL($("#myzar_category_add_icon")[0].files[0]);
+				isMyZarCategoryAddIconValid = true;
 			}
 			else {
 				$("#myzar_category_add_icon").val(null);
-				$("#myzar_category_add_icon_file").text('');
+				$("#myzar_category_add_msg").text('');
 				document.getElementById("myzar_category_add_icon_image").src = "image-solid.svg";
+				isMyZarCategoryAddIconValid = false;
+			}
+			myzar_category_add_button_update();
+		});
+		
+		$("#myzar_category_add_icon_title").on('input',function(e){
+			var tmpMyZarCategoryAddTitle = $("#myzar_category_add_icon_title").val().trim();
+			if(tmpMyZarCategoryAddTitle.length > 0){
+				if(tmpMyZarCategoryAddTitle != null){
+					isMyZarCategoryAddTitleValid = true;
+				}
+				else {
+					isMyZarCategoryAddTitleValid = false;	
+				}
+			}
+			else {
+				isMyZarCategoryAddTitleValid = false;
+			}
+			myzar_category_add_button_update();
+		});
+		
+		window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier('myzar_category_add_submit', {
+			'size': 'invisible',
+			'callback': function(response) {
+				firebase.auth().currentUser.getIdToken(/* forceRefresh */ true).then(function(idToken) {
+					
+					var myZarCategoryAddSubmitData = new FormData();
+					myZarCategoryAddSubmitData.append("file", $("#myzar_category_add_icon")[0].files[0]);
+					myZarCategoryAddSubmitData.append("uid", idToken);
+					myZarCategoryAddSubmitData.append("title", $("#myzar_category_add_icon_title").val().trim());
+					
+					const reqMyZarCategoryAddSubmit = new XMLHttpRequest();
+					reqMyZarCategoryAddSubmit.onload = function() {
+						if(this.responseText.includes("OK")){
+							console.log(this.responseText);
+//							$("#myzar_category_add_msg").text(this.responseText);
+//							$("#myzar_category_add_error").text('');
+						}
+						else {
+							$("#myzar_category_add_error").text(this.responseText);
+						}
+					};
+					reqMyZarCategoryAddSubmit.onerror = function(){
+						$("#myzar_category_add_error").text(reqMyZarCategoryAddSubmit.status);
+					};
+					reqMyZarCategoryAddSubmit.open("POST", "myzar_category_add_process.php");
+//					reqMyZarCategoryAddSubmit.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+//					reqMyZarCategoryAddSubmit.setRequestHeader("Content-Type", "multipart/form-data");
+					reqMyZarCategoryAddSubmit.setRequestHeader("X-Requested-With","XMLHttpRequest");
+					reqMyZarCategoryAddSubmit.send(myZarCategoryAddSubmitData);
+					console.log("Sent");
+				}).catch(function(error) {
+					$("#myzar_category_add_error").text(error);
+				});
 			}
 		});
+
+		recaptchaVerifier.render().then(function(widgetId) {
+			window.recaptchaWidgetId = widgetId;
+			myzar_category_add_button_update();
+		});
 	});
+	
+	function myzar_category_add_button_update(){
+		document.getElementById('myzar_category_add_submit').disabled = !isMyZarCategoryAddIconValid || !isMyZarCategoryAddTitleValid;
+	}
 	
 	function myzar_category_add_show(){
 		$("#myzar_category_add_popup").show();
@@ -28,24 +94,11 @@
 		$("#myzar_category_add_icon").trigger("click"); 
 	}
 	
-	function myzar_category_add_submit(){
-//		alert($('input[type=file]').val());	
-		//image/svg+xml
-//		alert("file:" + $("#myzar_category_add_icon")[0].files[0].name + ", " + $("#myzar_category_add_icon")[0].files[0].type);
-	}
-	
-//	window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier('sign-in-button', {
-//      'size': 'invisible',
-//      'callback': function(response) {
-//        // reCAPTCHA solved, allow signInWithPhoneNumber.
-//        onSignInSubmit();
-//      }
-//    });
-//
-//    recaptchaVerifier.render().then(function(widgetId) {
-//      window.recaptchaWidgetId = widgetId;
-//      updateSignInButtonUI();
-//    });
+//	function myzar_category_add_submit(){
+////		alert($('input[type=file]').val());	
+//		//image/svg+xml
+////		alert("file:" + $("#myzar_category_add_icon")[0].files[0].name + ", " + $("#myzar_category_add_icon")[0].files[0].type);
+//	}
 </script>
 
 <div class="myzar_content_category">
@@ -66,10 +119,10 @@
 	<div class="header">Ангилал</div>
 	<div style="display: flex; align-items: center; margin-left: 10px; height: 50px">
 		<img id="myzar_category_add_icon_image" src="image-solid.svg" width="24" height="24" onClick="myzar_category_add_icon_button()" />
-		<input type="text" maxlength="30" placeholder="Бичих..." style="margin-left: 5px" />
+		<input id="myzar_category_add_icon_title" type="text" maxlength="30" placeholder="Бичих..." style="margin-left: 5px" />
 	</div>
-	<div id="myzar_category_add_icon_file" class="msg"></div>
-	<div class="error" style="display: none">Error</div>
+	<div id="myzar_category_add_msg" class="msg"></div>
+	<div id="myzar_category_add_error" class="error"></div>
 	<input type="file" name="myzar_category_add_icon" id="myzar_category_add_icon" required="true" accept="image/png, .svg" />
-	<button onClick="myzar_category_add_submit()" disabled class="button_yellow" style="margin-top: 10px; margin-left: auto; margin-right: auto">Илгээх</button>
+	<button id="myzar_category_add_submit" disabled class="button_yellow" style="margin-top: 10px; margin-left: auto; margin-right: auto">Илгээх</button>
 </div>
