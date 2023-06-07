741614cde76b53d7373b88293b8e17a983bebbf7
Todbayar <atodko0513@gmail.com>
2023-06-07 10:42:44 +0800

    detail update

--- detail.php
+++ detail.php
@@ -1,6 +1,7 @@
 <?php
 include "mysql_config.php";
 include_once "mysql_misc.php";
+include_once "info.php";
 ?>
 
 <style>
@@ -49,7 +50,6 @@ include_once "mysql_misc.php";
 .button_yellow.price {
 	font-family: RobotoBold;
     font-size: 20px;
-	margin-top: 10px; 
 }
 	
 .button_yellow.phone {
@@ -69,10 +69,25 @@ include_once "mysql_misc.php";
 }
 
 .importantLeft {
-	max-width: 180px;
+	margin-top: 10px;
+	display: flex;
 }
-.importantLeft .favorite {
-	color: #a4a4a4;
+
+.importantLeft .owner {
+	margin-left: 10px;
+}
+	
+.owner .name {
+	font-size: 20px;
+	font-family: RobotoRegular;
+}
+	
+.owner .other_items {
+	margin-top: 5px;
+	font-size: 16px;
+	text-decoration: underline;
+	color: blue;
+	cursor: pointer;
 }
 
 .importantRight {
@@ -83,15 +98,57 @@ include_once "mysql_misc.php";
 	display: flex;
 }
 
+.detailMain .left {
+	width: 80%;
+}
+	
+.detailMain .left .images {
+	margin-top: 10px;
+}
+	
+.detailMain .left .images .big {
+	width: 100%;
+	height: 450px;
+	background: #e0e0e0;
+	border-radius: 10px;
+}
+
+.detailMain .left .images .big img {
+	width: 100%;
+	height: 450px;
+	border-radius: 10px;
+	object-fit: contain;
+}
+
+.detailMain .left .images .thumbnails {
+	margin-top: 5px;
+}
+	
+.detailMain .left .images .thumbnails img {
+	width: 80px;
+	height: 80px;
+	object-fit:cover;
+	border-radius: 5px;
+	cursor: pointer;
+}
+	
 .detailMain .right {
-	margin-left: 20px;
+	margin-left: 10px;
+	margin-right: 5px;
 }
 </style>
+
+<script>
+function showBigImage(id){
+	$("#imageBig").empty();
+	$("#imageBig").append("<img src=\""+$("#thumbnail"+id).attr("src")+"\" />");
+}
+</script>
 
 <div class="detail">
 <?php
 if(isset($_GET["id"])){
-	$query = "SELECT * FROM item WHERE id=".$_GET["id"];
+	$query = "SELECT *, item.phone AS item_phone FROM item RIGHT JOIN user ON user.id=item.userID WHERE item.id=".$_GET["id"];
 	$result = $conn->query($query);
 	$row = mysqli_fetch_array($result);
 	
@@ -113,7 +170,7 @@ if(isset($_GET["id"])){
 	
 	//rate calculate
 	$joinedCategories = implode(",",$arrCategories["id"]);
-	$queryRateList = "SELECT *, (SELECT COUNT(*) FROM item WHERE category IN (".$joinedCategories.")) AS count_category, (SELECT COUNT(*) FROM item) AS count_global, ((item_viewer+phone_viewer)/2) AS average, (((item_viewer+phone_viewer)/2)/(SELECT COUNT(*) FROM item WHERE category IN (".$joinedCategories."))) AS rate_category, (((item_viewer+phone_viewer)/2)/(SELECT COUNT(*) FROM item)) AS rate_global FROM item WHERE category IN (".$joinedCategories.") AND id=".$_GET["id"]." ORDER BY rate_category DESC, rate_global DESC";
+	echo $queryRateList = "SELECT *, (SELECT COUNT(*) FROM item WHERE category IN (".$joinedCategories.")) AS count_category, (SELECT COUNT(*) FROM item) AS count_global, ((item_viewer+phone_viewer)/2) AS average, (((item_viewer+phone_viewer)/2)/(SELECT COUNT(*) FROM item WHERE category IN (".$joinedCategories."))) AS rate_category, (((item_viewer+phone_viewer)/2)/(SELECT COUNT(*) FROM item)) AS rate_global FROM item WHERE category IN (".$joinedCategories.") AND id=".$_GET["id"]." ORDER BY rate_category DESC, rate_global DESC";
 	$resultRateList = $conn->query($queryRateList);
 	$rowRateList = mysqli_fetch_array($resultRateList);
 	?>
@@ -132,7 +189,7 @@ if(isset($_GET["id"])){
 	</div>
 	<div class="detailMain">
 		<div class="left">
-			<div class="title"><?php echo $row["title"]; ?></div>
+			<div class="title"><?php echo $row["title"]; ?> <i class="fa-regular fa-star" style="color:#a4a4a4"></i></div>
 			<div><?php echo $row["city"]; ?></div>
 			<div class="data1" style="margin-top: 5px; display: flex">
 				<div>Нийтэлсэн: <?php echo $row["datetime"]; ?></div>
@@ -143,25 +200,53 @@ if(isset($_GET["id"])){
 				<div style="margin-left: 5px">Эрэлт: <i class="fa-solid fa-star"></i> <?php echo number_format($rowRateList["rate_category"],1).", ".number_format($rowRateList["rate_global"],1); ?></div>
 			</div>
 			<div class="importantLeft">
-				<i class="favorite fa-regular fa-star"></i>
-				<div class="button_yellow price">
-					<div><?php echo convertPriceToText($row["price"]); ?> ₮</div>
-				</div>
-				<div class="button_yellow phone">
-					<i class="fa-solid fa-phone"></i>
-					<div style="margin-left: 10px">
-						<div>Дугаар харах</div>
-						<div class="hided" style="display: flex; font-family:RobotoRegular; font-size: 14px"><?php echo substr($row["phone"],4,4); ?>-<div style="color: #FFA718">XXXX</div></div>
+				<div>
+					<div class="button_yellow price">
+						<div><?php echo convertPriceToText($row["price"]); ?> ₮</div>
 					</div>
-					<div class="full" style="display: none; margin-left: 10px"><?php echo substr($row["phone"],4); ?></div>
+					<div class="button_yellow phone">
+						<i class="fa-solid fa-phone"></i>
+						<div style="margin-left: 10px">
+							<div>Дугаар харах</div>
+							<div class="hided" style="display: flex; font-family:RobotoRegular; font-size: 14px"><?php echo substr($row["item_phone"],4,4); ?>-<div style="color: #FFA718">XXXX</div></div>
+						</div>
+						<div class="full" style="display: none; margin-left: 10px"><?php echo substr($row["item_phone"],4); ?></div>
+					</div>
+					<div class="button_yellow chatlah" style="margin-top: 10px; background: #e60803">
+						<i class="fa-solid fa-comments"></i>
+						<div style="margin-left: 10px">Чатлах</div>
+					</div>
 				</div>
-				<div class="button_yellow chatlah" style="margin-top: 10px; background: #e60803">
-					<i class="fa-solid fa-comments"></i>
-					<div style="margin-left: 10px">Чатлах</div>
+				<div class="owner">
+					<div class="name"><?php echo $row["name"]; ?></div>
+					<div class="other_items">Зарын эзний бусад зарууд</div>
 				</div>
 			</div>
+			<?php
+			$queryImages = "SELECT * FROM images WHERE item=".$_GET["id"];
+			$resultImages = $conn->query($queryImages);
+			if(mysqli_num_rows($resultImages)>0){
+			?>
+			<div class="images">
+				<div id="imageBig" class="big"></div>
+				<div class="thumbnails">
+				<?php
+				$i = 1;
+				while($rowImages = mysqli_fetch_array($resultImages)){
+					?>
+					<img id="thumbnail<?php echo $i; ?>" onClick="showBigImage(<?php echo $i; ?>)" src="<?php echo $path; ?>/<?php echo $rowImages["image"]; ?>" />
+					<?php
+					$i++;
+				}
+				?>
+				<script>showBigImage(1);</script>
+				</div>
+			</div>
+			<?php
+			}
+			?>
 		</div>
-		<div class="right">
+		<div class="right removable">
 			<div class="importantRight">
 				<div class="button_yellow price">
 					<div><?php echo convertPriceToText($row["price"]); ?> ₮</div>
@@ -170,13 +255,17 @@ if(isset($_GET["id"])){
 					<i class="fa-solid fa-phone"></i>
 					<div style="margin-left: 10px">
 						<div>Дугаар харах</div>
-						<div class="hided" style="display: flex; font-family:RobotoRegular; font-size: 14px"><?php echo substr($row["phone"],4,4); ?>-<div style="color: #FFA718">XXXX</div></div>
+						<div class="hided" style="display: flex; font-family:RobotoRegular; font-size: 14px"><?php echo substr($row["item_phone"],4,4); ?>-<div style="color: #FFA718">XXXX</div></div>
 					</div>
-					<div class="full" style="display: none; margin-left: 10px"><?php echo substr($row["phone"],4); ?></div>
+					<div class="full" style="display: none; margin-left: 10px"><?php echo substr($row["item_phone"],4); ?></div>
 				</div>
 				<div class="button_yellow chatlah" style="margin-top: 10px; background: #e60803">
 					<i class="fa-solid fa-comments"></i>
 					<div style="margin-left: 10px">Чатлах</div>
+				</div>
+				<div class="owner" style="margin-top: 10px">
+					<div class="name"><?php echo $row["name"]; ?></div>
+					<div class="other_items">Зарын эзний бусад зарууд</div>
 				</div>
 			</div>
 		</div>
