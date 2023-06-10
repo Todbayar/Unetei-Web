8511c7d27e9bb3bafabf06bfd512fb0dab97e751
Todbayar <atodko0513@gmail.com>
2023-06-10 21:20:57 +0800

    topbar waves animation update green

--- index.php
+++ index.php
@@ -82,9 +82,9 @@ include "mysql_myzar_item_remove_process.php";	//for auto removal of expired ite
 						<path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
 					</defs>
 					<g class="parallax">
-						<use xlink:href="#gentle-wave" x="40" y="0" fill="rgba(255,167,64,0.7" />
-						<use xlink:href="#gentle-wave" x="48" y="3" fill="rgba(0,125,197,0.7)" />
-						<use xlink:href="#gentle-wave" x="48" y="5" fill="rgba(255,74,151,0.7)" />
+						<use xlink:href="#gentle-wave" x="48" y="0" fill="rgba(55,163,0,0.7" />
+						<use xlink:href="#gentle-wave" x="48" y="3" fill="rgba(55,163,0,0.5)" />
+						<use xlink:href="#gentle-wave" x="48" y="5" fill="rgba(55,163,0,0.3)" />
 						<use xlink:href="#gentle-wave" x="48" y="7" fill="#fff" />
 					</g>
 				</svg>
