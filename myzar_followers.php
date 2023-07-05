d972d0e4512bf9383371f597815db5afb29c3c5f
Todbayar <atodko0513@gmail.com>
2023-07-05 14:30:59 +0800

    showed indirect followers for super duper admin

--- myzar_followers.php
+++ myzar_followers.php
@@ -8,16 +8,24 @@ include_once "mysql_misc.php";
 .myzar_followers {
 	padding-top: 10px;
 	padding-bottom: 10px;
+	width: 100%;
+	float: left;
 }
 
-.follower {
+.myzar_followers .container {
+	width: 100%;
+	float: left;
+	display:block;
+}
+	
+.myzar_followers .container .followers_list {
 	text-align: center;
 	float: left;
 	margin: 10px;
 	cursor: pointer;
 }
 	
-.follower .image {
+.myzar_followers .container .followers_list .image {
 	width: 82px;
 	height: 82px;
 	border-radius: 100%;
@@ -59,21 +67,54 @@ function showOtherItems(userID){
 	$resultFollowers = $conn->query($queryFollowers);
 	while($rowFollowers = mysqli_fetch_array($resultFollowers)){
 	?>
-	<div class="follower">
-		<img class="image" src="<?php echo $path."/".$rowFollowers["image"]; ?>" onerror="this.onerror=null; this.src='user.png'" />
-		<div class="name"><?php echo $rowFollowers["name"]; ?></div>
-		<div class="role"><?php echo convertRoleInString($rowFollowers["role"]); ?></div>
-		<div class="phone"><?php echo substr($rowFollowers["phone"],4); ?></div>
-		<div onclick="startChat(<?php echo $rowFollowers["id"]; ?>,'Сайн байна уу?')" class="button_yellow" style="margin-top: 5px">
-			<i class="fa-solid fa-comments"></i>
-			<div style="margin-left: 5px">Чатлах</div>
+	<div class="container">
+		<div style="font-family: RobotoBold; margin-left: 10px">
+			Шууд дагагчид <a style="color: gray; font-family: RobotoRegular; font-size: 14px">(Таны утасны дугаараар дагагч болсон хүмүүс)</a>
 		</div>
-		<div onClick="showOtherItems(<?php echo $rowFollowers["id"]; ?>)" class="button_yellow" style="margin-top: 5px">
-			<i class="fa-solid fa-cart-shopping"></i>
-			<div style="margin-left: 5px">Зарууд</div>
+		<hr/>
+		<div class="followers_list">
+			<img class="image" src="<?php echo $path."/".$rowFollowers["image"]; ?>" onerror="this.onerror=null; this.src='user.png'" />
+			<div class="name"><?php echo $rowFollowers["name"]; ?></div>
+			<div class="role"><?php echo convertRoleInString($rowFollowers["role"]); ?></div>
+			<div class="phone"><?php echo substr($rowFollowers["phone"],4); ?></div>
+			<div onclick="startChat(<?php echo $rowFollowers["id"]; ?>,'Сайн байна уу?')" class="button_yellow" style="margin-top: 5px">
+				<i class="fa-solid fa-comments"></i>
+				<div style="margin-left: 5px">Чатлах</div>
+			</div>
+			<div onClick="showOtherItems(<?php echo $rowFollowers["id"]; ?>)" class="button_yellow" style="margin-top: 5px">
+				<i class="fa-solid fa-cart-shopping"></i>
+				<div style="margin-left: 5px">Зарууд</div>
+			</div>
 		</div>
 	</div>
 	<?php
+	}
+	
+	if(getPhone($_COOKIE["userID"])==$superduperadmin){
+		$queryFollowers = "SELECT * FROM user WHERE affiliate='' AND phone!='".$superduperadmin."'";
+		$resultFollowers = $conn->query($queryFollowers);
+		while($rowFollowers = mysqli_fetch_array($resultFollowers)){
+		?>
+		<div class="container" style="margin-top: 10px">
+			<div style="font-family: RobotoBold; margin-left: 10px">Шууд бус дагагчид</div>
+			<hr/>
+			<div class="followers_list">
+				<img class="image" src="<?php echo $path."/".$rowFollowers["image"]; ?>" onerror="this.onerror=null; this.src='user.png'" />
+				<div class="name"><?php echo $rowFollowers["name"]; ?></div>
+				<div class="role"><?php echo convertRoleInString($rowFollowers["role"]); ?></div>
+				<div class="phone"><?php echo substr($rowFollowers["phone"],4); ?></div>
+				<div onclick="startChat(<?php echo $rowFollowers["id"]; ?>,'Сайн байна уу?')" class="button_yellow" style="margin-top: 5px">
+					<i class="fa-solid fa-comments"></i>
+					<div style="margin-left: 5px">Чатлах</div>
+				</div>
+				<div onClick="showOtherItems(<?php echo $rowFollowers["id"]; ?>)" class="button_yellow" style="margin-top: 5px">
+					<i class="fa-solid fa-cart-shopping"></i>
+					<div style="margin-left: 5px">Зарууд</div>
+				</div>
+			</div>
+		</div>
+		<?php
+		}
 	}
 	?>
 </div>
